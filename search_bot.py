f2814276435651789b8cbc2d17d31f7658dc0755
heyyyjonardon <57803702+heyyyjonardon@users.noreply.github.com>
2020-02-08 22:17:53 +0530

    Add files via upload

---
+++ search_bot.py
@@ -0,0 +1,45 @@
+import pyperclip,schedule,webbrowser
+from selenium import webdriver
+
+class SearchBot():
+
+    def __init__(self):
+        
+        self.driver = webdriver.Chrome('E:\PythonFiles\chromedriver_win32 79\chromedriver.exe')
+    
+    def google_search(self):
+
+        self.driver.get("https://google.com")
+        
+        search_button = self.driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+        
+        search_button.click()
+        
+        search_query = pyperclip.paste()
+        
+        search_button.send_keys(search_query)
+        
+        search_submit = self.driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]')
+        
+        search_submit.click()
+ 
+
+
+def do():
+    while True:
+        old_search = pyperclip.paste()
+
+        Qbot = SearchBot()
+        Qbot.google_search()
+        while True:
+            newmaybe = pyperclip.paste()
+            if newmaybe == old_search:
+                continue
+            elif newmaybe!=old_search:
+                break
+do()
+
+
+
+
+
